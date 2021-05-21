12d6cfbfe62a0fb58fff837997935c60fc0eb214
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-21 19:25:44 +0530

    Linting changes

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <label>Title</label>
+  <input v-model="event.title" type="text" placeholder="Title" class="field" />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
