26dca22036cd27250390051605f69deaf148544e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-21 19:46:32 +0530

    BaseInput Component

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,13 +1,24 @@
 <template>
-  <label>Title</label>
-  <input v-model="event.title" type="text" placeholder="Title" class="field" />
+  <label>{{ label }}</label>
+  <input
+    class="field"
+    :placeholder="`Enter ${label}`"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+  />
 </template>
 
 <script>
 export default {
+  name: "BaseInput",
   props: {
     label: {
       type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
       default: "",
     },
   },
