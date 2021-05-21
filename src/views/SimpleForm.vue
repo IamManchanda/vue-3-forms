19ebee36c8f7662681f2082340c96b2f1f98db26
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-21 21:36:34 +0530

    Form Submission

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <form>
+    <form @submit.prevent="sendForm">
       <BaseSelect
         :options="categories"
         v-model="event.category"
@@ -38,6 +38,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "SimpleForm",
   data() {
@@ -67,6 +69,21 @@ export default {
         { label: "No", value: 0 },
       ],
     };
+  },
+  methods: {
+    sendForm() {
+      axios
+        .post(
+          "https://my-json-server.typicode.com/IamManchanda/vue-3-forms/events",
+          this.event
+        )
+        .then(function (response) {
+          console.log("response:", response);
+        })
+        .catch(function (err) {
+          console.log("error:", err);
+        });
+    },
   },
 };
 </script>
